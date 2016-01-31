3a6fe0a5cb8526919f915bae451e1794ccbf0477
gerifield <radics.gergely@ustream.tv>
2016-01-31 22:55:50 +0100

    Added backend handling

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -15,6 +16,54 @@ type Config struct {
 	Frontend string   `yaml:"frontend"`
 	Backends []string `yaml:"backends"`
 }
+
+type Backend struct {
+	Addr     string
+	Network  string
+	OpenConn int
+	Alive    bool
+	LastConn time.Time
+
+	BackendLock sync.Mutex
+}
+
+func NewBackend(addr string) *Backend {
+	return &Backend{
+		Addr:        addr,
+		Network:     "tcp",
+		OpenConn:    0,
+		Alive:       false,
+		BackendLock: sync.Mutex{},
+	}
+}
+
+func (b *Backend) UpdateAlive() {
+	c, err := net.Dial(b.Network, b.Addr)
+	if err != nil {
+		b.BackendLock.Lock()
+		b.Alive = false
+		b.BackendLock.Unlock()
+		return
+	}
+	c.Close()
+	b.BackendLock.Lock()
+	b.Alive = true
+	b.BackendLock.Unlock()
+}
+
+func (b *Backend) UpdateConn(i int) {
+	b.BackendLock.Lock()
+	b.OpenConn += i
+	b.BackendLock.Unlock()
+}
+
+func (b *Backend) UpdateLastConn() {
+	b.BackendLock.Lock()
+	b.LastConn = time.Now()
+	b.BackendLock.Unlock()
+}
+
+var backends []*Backend
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -31,6 +80,11 @@ func main() {
 		log.Println(err)
 		return
 	}
+	for _, b := range conf.Backends {
+		bck := NewBackend(b)
+		bck.UpdateAlive()
+		backends = append(backends, bck)
+	}
 
 	l, err := net.Listen("tcp", conf.Frontend)
 	if err != nil {
@@ -43,25 +97,28 @@ func main() {
 		if err != nil {
 			log.Println(err)
 		}
-		go handleConnection(conn, conf)
+		go handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn, conf Config) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	var err error
-	var bck string
+	var bck *Backend
 	var bckConn net.Conn
-	maxLoop := len(conf.Backends) //Max loop limit
+	maxLoop := len(backends) //Max loop limit
 	loopCount := 0
 
 	for { //Try until successful connection
-		bck = conf.Backends[rand.Intn(len(conf.Backends))]
-		bckConn, err = net.Dial("tcp", bck)
+
+		bck = ChooseBackend(backends, "random")
+
+		bckConn, err = net.Dial(bck.Network, bck.Addr)
 		if err != nil {
 			log.Println(err)
 			if maxLoop >= loopCount {
 				log.Println("Loop limit reached!")
+				bck.UpdateAlive() //Maybe it's not alive
 				return
 			}
 			loopCount++
@@ -69,7 +126,21 @@ func handleConnection(conn net.Conn, conf Config) {
 			break //Conn ok, break the loop
 		}
 	}
+	bck.UpdateConn(1)
+	bck.UpdateLastConn()
+	defer bck.UpdateConn(-1)
+	defer bckConn.Close()
+
+	log.Println(bck, bck.OpenConn)
 
 	go io.Copy(bckConn, conn)
 	io.Copy(conn, bckConn)
+}
+
+func ChooseBackend(backends []*Backend, alg string) *Backend {
+	switch alg {
+	case "random":
+		return backends[rand.Intn(len(backends))]
+	}
+	return nil
 }
